70fd5168495d6f19737c905b43020bb675898c9e
eric <higuoxing@outlook.com>
2017-08-02 18:30:21 +0800

    Update FREQ_meter.v

--- FREQ_meter.v
+++ FREQ_meter.v
@@ -20,8 +20,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module FREQ_meter(
-    input wire sys_clk,          // -- sys_clk = 200MHz
+module FREQ_measure(
+    input wire clk_200M,          // -- clk_200M = 200MHz
     input wire rst_n,
     input wire sig_in,
     output reg [31:0] sig_freq_cnt_buf,
@@ -50,6 +50,7 @@ module FREQ_meter(
     reg real_gate_pos_detect_r1;
     wire real_gate_neg_detect;
     
+//    wire clk_200M;
     reg ref_clk_100MHz;
     reg [31:0] ref_clk_100MHz_cnt;
     reg ref_clk_100MHz_pos_detect_r0;
@@ -60,6 +61,7 @@ module FREQ_meter(
     
     reg buffer_done;
     reg [31:0] buffer_done_cnt;
+    
     
     initial begin
         sig_freq_cnt <= 32'd0;
@@ -82,11 +84,15 @@ module FREQ_meter(
         
         sig_in_pos_detect_r0 <= 1'b0;
         sig_in_pos_detect_r1 <= 1'b0;
+        
+        buffer_done <= 1'b0;
+        buffer_done_cnt <= 32'd0;
     end
     
     // -- ref gate 0.5Hz clock generator
-    always @ (posedge sys_clk or negedge rst_n) begin
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
+            ref_gate <= 1'b0;
             ref_gate_cnt <= 32'd0;
         end
         else if (ref_gate_cnt == max_ref_gate_cnt) begin
@@ -97,24 +103,26 @@ module FREQ_meter(
     end
     
     // -- ref 100MHz clock generator
-    always @ (posedge sys_clk or negedge rst_n) begin
-        if (!rst_n) begin
-            ref_clk_100MHz <= 1'b0;
-            ref_clk_100MHz_cnt <= 32'd0;
-        end
-        else if (ref_clk_100MHz_cnt == max_ref_clk_100MHz_cnt) begin
-            ref_clk_100MHz <= ~ ref_clk_100MHz;
-            ref_clk_100MHz_cnt <= 32'd0;
-        end
-        else ref_clk_100MHz_cnt <= ref_clk_100MHz_cnt + 1;
-    end
+//    always @ (posedge clk_200M or negedge rst_n) begin
+//        if (!rst_n) begin
+//            ref_clk_100MHz <= 1'b0;
+//            ref_clk_100MHz_cnt <= 32'd0;
+//        end
+//        else if (ref_clk_100MHz_cnt == max_ref_clk_100MHz_cnt) begin
+//            ref_clk_100MHz <= ~ ref_clk_100MHz;
+//            ref_clk_100MHz_cnt <= 32'd0;
+//        end
+//        else ref_clk_100MHz_cnt <= ref_clk_100MHz_cnt + 1;
+//    end
+
+    always @ (posedge clk_200M) ref_clk_100MHz <= ~ ref_clk_100MHz;
     
     // -- sig_in posedge detect
     assign sig_in_pos_detect = (sig_in_pos_detect_r0 && 
                                !sig_in_pos_detect_r1) ? 1'b1: 1'b0;
     assign sig_in_neg_detect = (!sig_in_pos_detect_r0 &&
-                             sig_in_pos_detect_r0) ? 1'b1: 1'b0;
-    always @ (posedge sys_clk or negedge rst_n) begin
+                                sig_in_pos_detect_r1) ? 1'b1: 1'b0;
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             sig_in_pos_detect_r0 <= 1'b0;
             sig_in_pos_detect_r1 <= 1'b0;
@@ -127,8 +135,8 @@ module FREQ_meter(
     
     // -- ref 100MHz clock posedge detect
     assign ref_clk_100MHz_pos_detect = (ref_clk_100MHz_pos_detect_r0 &&
-                                        !ref_clk_100MHz_pos_detect_r1) ? 1'b1: 1'b0;
-    always @ (posedge sys_clk or negedge rst_n) begin
+                                      !ref_clk_100MHz_pos_detect_r1) ? 1'b1: 1'b0;
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             ref_clk_100MHz_pos_detect_r0 <= 1'b0;
             ref_clk_100MHz_pos_detect_r1 <= 1'b0;
@@ -144,7 +152,7 @@ module FREQ_meter(
                                         !ref_gate_pos_detect_r1) ? 1'b1: 1'b0;
     assign ref_gate_neg_detect = (!ref_gate_pos_detect_r0 &&
                                         ref_gate_pos_detect_r1) ? 1'b1: 1'b0;                                       
-    always @ (posedge sys_clk or negedge rst_n) begin
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             ref_gate_pos_detect_r0 <= 1'b0;
             ref_gate_pos_detect_r1 <= 1'b0;
@@ -156,7 +164,7 @@ module FREQ_meter(
     end
     
     // -- real gate generator
-    always @ (posedge sys_clk or negedge rst_n) begin
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             real_gate <= 1'b0;
         end
@@ -169,8 +177,22 @@ module FREQ_meter(
         else real_gate <= real_gate;
     end
     
+    // -- real gate posedge& negedge detect
+    assign real_gate_neg_detect =  (!real_gate_pos_detect_r0 && 
+                                    real_gate_pos_detect_r1) ? 1'b1: 1'b0;
+    always @ (posedge clk_200M or negedge rst_n) begin
+        if (!rst_n) begin
+            real_gate_pos_detect_r0 <= 1'b0;
+            real_gate_pos_detect_r1 <= 1'b0;
+        end
+        else begin
+            real_gate_pos_detect_r0 <= real_gate;
+            real_gate_pos_detect_r1 <= real_gate_pos_detect_r0;
+        end
+    end
+    
     // -- ref_100MHz clock counter
-    always @ (posedge sys_clk or negedge rst_n) begin
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             ref_clk_cnt <= 32'd0;
         end
@@ -184,30 +206,30 @@ module FREQ_meter(
     end
     
     // -- sig_in paulse counter
-    always @ (posedge sys_clk or negedge rst_n) begin
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             sig_freq_cnt <= 32'd0;
         end
         else if (real_gate && sig_in_pos_detect) begin
             sig_freq_cnt <= sig_freq_cnt + 1;
         end
-        else if (!real_gate && buffer_done) begin
+        else if (!real_gate && buffer_done ) begin
             sig_freq_cnt <= 32'd0;
         end
         else sig_freq_cnt <= sig_freq_cnt;
     end
     
     // -- set synchronizing data flag
-    always @ (posedge sys_clk or negedge rst_n) begin
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             buffer_done <= 1'b0;
             buffer_done_cnt <= 32'd0;
         end
-        else if (!real_gate && buffer_done_cnt != 10) begin
+        else if (!real_gate && buffer_done_cnt != 20) begin
             buffer_done_cnt <= buffer_done_cnt + 1;
             buffer_done <= 1'b0;
         end
-        else if (!real_gate && buffer_done_cnt == 10) begin
+        else if (!real_gate && buffer_done_cnt == 20) begin
             buffer_done_cnt <= buffer_done_cnt;
             buffer_done <= 1'b1;
         end
@@ -222,12 +244,12 @@ module FREQ_meter(
     end
     
     // -- synchronize data
-    always @ (posedge sys_clk or negedge rst_n) begin
+    always @ (posedge clk_200M or negedge rst_n) begin
         if (!rst_n) begin
             sig_freq_cnt_buf <= 32'd0;
             ref_clk_cnt_buf <= 32'd0;
         end
-        else if (ref_gate_neg_detect) begin
+        else if (real_gate_neg_detect) begin
             sig_freq_cnt_buf <= sig_freq_cnt;
             ref_clk_cnt_buf <= ref_clk_cnt;
         end
